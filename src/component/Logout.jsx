42b6af681a6e2cb508cfd12b95b52d2ad67506b1
Your Name <your.email@example.com>
2026-08-04 16:20:08 +0500

    update a login api

--- src/component/Logout.jsx
+++ src/component/Logout.jsx
@@ -1,17 +1,15 @@
-import axios from 'axios'
-import { LogOut } from 'lucide-react'
-import React from 'react'
-import { Navigate } from 'react-router-dom'
-import { toast, ToastContainer, Zoom } from 'react-toastify'
+import axios from "axios";
+import { LogOut } from "lucide-react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, Zoom } from "react-toastify";
 
 const Logout = () => {
-
-  const navigate = Navigate;
-
+  const navigate = useNavigate() 
   async function logoutUser() {
     try {
-      const res = await axios.post(
-        import.meta.env.VITE_SIGIN_OUT_URL,
+      await axios.post(
+        import.meta.env.VITE_SIGN_OUT_URL,
         {},
         { withCredentials: true }
       );
@@ -24,25 +22,22 @@ const Logout = () => {
 
   return (
     <>
-      <button onClick={logoutUser} className='p-4 btn active:scale-95 btn-color text-white rounded-2xl flex gap-3'>
-        <LogOut /><span className='md:flex hidden'>Logout</span>
+      <button
+        onClick={logoutUser}
+        className="p-4 btn active:scale-95 btn-color text-white rounded-2xl flex gap-3"
+      >
+        <LogOut />
+        <span className="md:flex hidden">Logout</span>
       </button>
+
       <ToastContainer
         position="top-right"
         autoClose={3000}
         hideProgressBar
-        newestOnTop={false}
-        closeOnClick={false}
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
         transition={Zoom}
       />
     </>
+  );
+};
 
-  )
-}
-
-export default Logout
+export default Logout;

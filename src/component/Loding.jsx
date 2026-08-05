1c794d3b90c017979f5d077445b08919545764c7
Your Name <your.email@example.com>
2026-08-05 16:31:42 +0500

    update loder

--- src/component/Loding.jsx
+++ src/component/Loding.jsx
@@ -1,22 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-const Loding = () => {
+const Loading = () => {
   return (
-    <div className="absolute top-2/4 left-2/4 -translate-x-2/4 w-full h-full bg-white/70 -translate-y-2/4 flex justify-center items-center">
-      <div className="w-full gap-x-2 flex justify-center items-center">
-        <div
-          className="w-5 bg-[#d991c2] h-5 rounded-full animate-bounce"
-        ></div>
-        <div
-          className="w-5 animate-pulse h-5 bg-[#9869b8] rounded-full"
-        ></div>
-        <div
-          className="w-5 h-5 animate-pulse bg-[#6756cc] rounded-full"
-        ></div>
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <div className="flex gap-x-2">
+        <div className="w-5 h-5 bg-[#d991c2] rounded-full animate-bounce"></div>
+        <div className="w-5 h-5 bg-[#9869b8] rounded-full animate-pulse"></div>
+        <div className="w-5 h-5 bg-[#6756cc] rounded-full animate-pulse"></div>
       </div>
 
+      <span className="mt-4 text-gray-600 font-medium">
+        Please wait...
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default Loding
+export default Loading;
